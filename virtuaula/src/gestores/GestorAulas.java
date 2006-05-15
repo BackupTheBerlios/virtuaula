d8832a9c4829c8fb60f6cb9a86189549ce400465
sergiois <sergiois>
2006-05-15 21:53:00 +0000

    He añadido el método comprobar

--- virtuaula/src/gestores/GestorAulas.java
+++ virtuaula/src/gestores/GestorAulas.java
@@ -1,9 +1,12 @@
 package gestores;
 
 import subSistemaBBDD.BBDDFachada;
+import subSistemaBBDD.utils.Constantes;
 import beans.Aula;
 import beans.CreadorBean;
+import beans.Error;
 import beans.ObjetoBean;
+import beans.listaObjetoBeans.CreadorListaObjetoBean;
 import beans.listaObjetoBeans.ListaObjetoBean;
 
 public class GestorAulas {
@@ -26,4 +29,37 @@ public class GestorAulas {
 		return l;
 	}
 	
+	private ListaObjetoBean comprobar(ObjetoBean bean) {
+		CreadorListaObjetoBean c = new CreadorListaObjetoBean();
+		CreadorBean cBean = new CreadorBean();
+		ListaObjetoBean listaerror = c.crear();
+		String mensaje = "";
+		int i = 0;
+		
+
+		//La capacidad debe ser un entero mayor que 0
+		if (!bean.dameValor(Constantes.AULA_CAPACIDAD).equals("")){
+			try {
+				int cap = Integer.parseInt(bean.dameValor(Constantes.AULA_CAPACIDAD));
+				if (cap < 0) {
+					mensaje = "El campo Capacidad tiene un valor incorrecto. " +
+							  "Debe ser un entero mayor que 0";
+					Error error = (Error) cBean.crear(14);
+					error.cambiaValor("CAUSA_ERROR", mensaje);
+					listaerror.insertar(i, error);
+					i++;
+				}
+			  } 
+			  catch (Exception e) {
+				// No es número
+				mensaje = "El campo Capacidad tiene un valor incorrecto. Debe ser un" +
+						  "entero mayor que 0";
+				Error error = (Error) cBean.crear(14);
+				error.cambiaValor("CAUSA_ERROR", mensaje);
+				listaerror.insertar(i, error);
+				i++;
+			  }
+		}
+		return listaerror;
+	}
 }
