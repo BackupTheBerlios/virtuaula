c815f6049741ed7ca2c01aa8fc3ff04d0828c09e
sergiois <sergiois>
2006-05-15 18:18:20 +0000

    He añadido el método comprobar.

--- virtuaula/src/gestores/Profesorado.java
+++ virtuaula/src/gestores/Profesorado.java
@@ -1,9 +1,12 @@
 package gestores;
 
 import subSistemaBBDD.BBDDFachada;
+import subSistemaBBDD.utils.Constantes;
 import beans.CreadorBean;
+import beans.Error;
 import beans.ObjetoBean;
 import beans.Profesor;
+import beans.listaObjetoBeans.CreadorListaObjetoBean;
 import beans.listaObjetoBeans.ListaObjetoBean;
 
 public class Profesorado {
@@ -42,5 +45,55 @@ public class Profesorado {
 		ListaObjetoBean l = bdf.dameCursosProfesor(beanProfesor);
 		return l;
 	}
+	
+	private ListaObjetoBean comprobar(ObjetoBean bean) {
+		CreadorListaObjetoBean c = new CreadorListaObjetoBean();
+		CreadorBean cBean = new CreadorBean();
+		ListaObjetoBean listaerror = c.crear();
+		String mensaje = "";
+		int i = 0;
+		
+		//Se comprueba que los campos obligatorios tienen datos
 
+		//El DNI no debe ser un campo vacío
+		if (bean.dameValor(Constantes.ID_ISPROFESOR_ISUSUARIO_DNI).equals("")){
+			mensaje = "El campo DNI no ha sido rellenado";
+			Error error = (Error) cBean.crear(14);
+			error.cambiaValor("CAUSA_ERROR", mensaje);
+			listaerror.insertar(i, error);
+			i++;
+		}
+		//El área no debe ser un campo vacío
+		else if (bean.dameValor(Constantes.PROFESOR_ISAREA_IDISAREA).equals("")){
+			mensaje = "El campo Area no ha sido rellenado";
+			Error error = (Error) cBean.crear(14);
+			error.cambiaValor("CAUSA_ERROR", mensaje);
+			listaerror.insertar(i, error);
+			i++;
+		}
+		//El telefono debe ser un entero mayor que 0
+		else if (!bean.dameValor(Constantes.PROFESOR_TELEFONO).equals("")){
+		  try {
+			int telf = Integer.parseInt(bean.dameValor(Constantes.PROFESOR_TELEFONO));
+			if (telf < 0) {
+				mensaje = "El campo Telefono tiene un valor incorrecto. Debe ser un" +
+						  "entero mayor que 0";
+				Error error = (Error) cBean.crear(14);
+				error.cambiaValor("CAUSA_ERROR", mensaje);
+				listaerror.insertar(i, error);
+				i++;
+			}
+		  } 
+		  catch (Exception e) {
+			// No es número
+			mensaje = "El campo Telefono tiene un valor incorrecto. Debe ser un" +
+					  "entero mayor que 0";
+			Error error = (Error) cBean.crear(14);
+			error.cambiaValor("CAUSA_ERROR", mensaje);
+			listaerror.insertar(i, error);
+			i++;
+		  }
+		}
+		return listaerror;
+	}
 }
