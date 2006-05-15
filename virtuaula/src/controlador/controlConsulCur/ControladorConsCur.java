fe3414f376605e5329344947c79db21d554f5b35
sergiois <sergiois>
2006-05-15 16:19:04 +0000

    Version inicial

---
+++ virtuaula/src/controlador/controlConsulCur/ControladorConsCur.java
@@ -0,0 +1,43 @@
+package controlador.controlConsulCur;
+
+import beans.listaObjetoBeans.ListaObjetoBean;
+import beans.*;
+import controlador.Controlador;
+import gestores.GestorCursos;
+
+/**
+ * 
+ * @author Sergio Piqueras Martínez
+ * Este controlador controla la navegación entre dos paginas
+ * en la consulta de un curso. En este paso consultará los cursos que cumplen las
+ * características de los datos introducidos en la página anterior para poder 
+ * mostrarlos en la pagina siguiente.
+ *
+ */
+public class ControladorConsCur extends Controlador{
+
+	/**
+	 * Constructora de la clase.
+	 */
+	public ControladorConsCur() {
+		super();
+	}
+	/**
+	 * Este metodo nos modifica el resultadooperacion para indicar a la pagina que
+	 * tiene que pasar. Ademas utilizando el gestor de cursos consulta los cursos con
+	 * el bean que le pasamos por la session.
+	 */
+		public void procesarEvento() {
+			Curso curso = (Curso)this.getSesion().getAttribute("beancurso");
+			//ListaObjetoBean	listacurso=this.gestorCursos.consultaCursos(curso);
+			GestorCursos gestor = new GestorCursos();
+			ListaObjetoBean	listacurso=gestor.consultaCurso(curso);
+			this.getSesion().setAttribute("listacurso",listacurso);
+			
+			if (listacurso!=null)this.setResuladooperacion("OK");
+			else if (listacurso==null)this.setResuladooperacion("ERROR");
+		}
+		public Controlador clonar(){
+			return new ControladorConsCur();
+		}
+}
---
+++ virtuaula/src/controlador/controlConsulCur/ControladorMostrarCur.java
@@ -0,0 +1,47 @@
+package controlador.controlConsulCur;
+
+import beans.listaObjetoBeans.ListaObjetoBean;
+import beans.*;
+import controlador.Controlador;
+import gestores.GestorCursos;
+
+/**
+ * 
+ * @author Sergio Piqueras Martínez
+ * Este controlador controla la navegación entre dos paginas
+ * en la consulta de un curso. En este paso buscará el profesor, horario, aula y área
+ * que tiene el curso seleccionado y los pasa a la session.
+ *
+ */
+public class ControladorMostrarCur extends Controlador{
+
+	/**
+	 * Constructora de la clase.
+	 */
+	public ControladorMostrarCur() {
+		super();
+	}
+	/**
+	 * Este metodo nos modifica el resultadooperacion para indicar a la pagina que
+	 * tiene que pasar. Ademas utilizando el gestor de cursos consulta los cursos con
+	 * el bean que le pasamos por la session.
+	 */
+		public void procesarEvento() {
+			ListaObjetoBean	listacurso=(ListaObjetoBean)this.getSesion().getAttribute("listacurso");
+			Curso curso=(Curso)listacurso.dameObjeto(Integer.valueOf((String)this.sesion.getAttribute("posCurso")).intValue());
+			GestorCursos gestor = new GestorCursos();
+			Area area=gestor.consultaAreaDeCurso(curso);
+			Aula aula=gestor.consultaAulaDeCurso(curso);
+			Profesor profesor=gestor.consultaProfesorDeCurso(curso);
+			Horario horario=gestor.consultaHorarioDeCurso(curso);
+			this.getSesion().setAttribute("beanaula",aula);
+			this.getSesion().setAttribute("beanarea",area);
+			this.getSesion().setAttribute("beanprofesor",profesor);
+			this.getSesion().setAttribute("beanhorario",horario);
+			this.setResuladooperacion("SIG");
+			}
+		public Controlador clonar(){
+			return new ControladorMostrarCur();
+		}
+
+}
