4931a6acdf4e7d67230d6deb0837070d8e884c90
jmusulin <jmusulin>
2006-05-25 20:48:12 +0000

    Actualizacion para que funcione el nuevo metodo de la fachada porque el anterior tenia problemas con el jdk1.4.x

--- virtuaula/src/subSistemaBBDD/utils/Constantes.java
+++ virtuaula/src/subSistemaBBDD/utils/Constantes.java
@@ -7,13 +7,13 @@ public class Constantes {
 	public static final String ERROR_DESCONEXION_BBDD = "Se ha produciodo un error al desconectar de la BBDD";
 	
 	//ISAREA
-	public static final String objetoArea="IsArea";
+	public static final String objetoArea="subSistemaBBDD.objetoBaseDatos.IsArea";
 	public static final String TABLA_AREA = "isarea";
 	public static final String ID_ISAREA = "IDISAREA";
 	public static final String AREA_NOMBRE = "NOMBRE";
 	
 	//ISAULA
-	public static final String objetoAula="IsAula";
+	public static final String objetoAula="subSistemaBBDD.objetoBaseDatos.IsAula";
 	public static final String TABLA_AULA = "isaula";
 	public static final String ID_ISAULA = "IDISAULA";
 	public static final String AULA_NOMBRE = "NOMBRE";
@@ -21,7 +21,7 @@ public class Constantes {
 	public static final String AULA_LOCALIZACION = "LOCALIZACION";
 	
 	//ISAVISO
-	public static final String objetoAviso="IsAvisos";
+	public static final String objetoAviso="subSistemaBBDD.objetoBaseDatos.IsAvisos";
 	public static final String TABLA_AVISOS = "isavisos";
 	public static final String ID_ISAVISOS = "IDISAVISOS";
 	public static final String AVISOS_FECHA_AVISO = "FECHA_AVISO";
@@ -31,14 +31,14 @@ public class Constantes {
 	public static final String AVISOS_ACTIVO = "ACTIVO";
 	
 	//ISNOMINA
-	public static final String objetoNomina="IsNomina";
+	public static final String objetoNomina="subSistemaBBDD.objetoBaseDatos.IsNomina";
 	public static final String TABLA_NOMINA = "isnomina";
 	public static final String ID_ISNOMINA = "IDISNOMINA";
 	public static final String NOMINA_CUENTA_INGRESOS = "CUENTA_INGRESOS";
 	public static final String NOMINA_CANTIDAD = "CANTIDAD";
 	
 	//ISHORARIO
-	public static final String objetoHorario="IsHorario";
+	public static final String objetoHorario="subSistemaBBDD.objetoBaseDatos.IsHorario";
 	public static final String TABLA_HORARIO = "ishorario";
 	public static final String ID_ISHORARIO = "IDISHORARIO";
 	public static final String HORARIO_LUNES = "L";
@@ -48,7 +48,7 @@ public class Constantes {
 	public static final String HORARIO_VIERNES = "V";
 	
 	//ISFICHA
-	public static final String objetoFicha="IsFicha";
+	public static final String objetoFicha="subSistemaBBDD.objetoBaseDatos.IsFicha";
 	public static final String TABLA_FICHA = "isficha";
 	public static final String ID_ISFICHA = "IDISFICHA";
 	public static final String FICHA_NOTAS = "NOTAS";
@@ -56,21 +56,21 @@ public class Constantes {
 	public static final String FICHA_NOTAS_EJERCICIOS = "NOTAS_EJERCICIOS";
 		
 	//ISUSUARIO
-	public static final String objetoUsuario="IsUsuario";
+	public static final String objetoUsuario="subSistemaBBDD.objetoBaseDatos.IsUsuario";
 	public static final String TABLA_USUARIO = "isusuario";
 	public static final String ID_ISUSUARIO_DNI = "DNI";
 	public static final String USUARIO_CONTRASENIA = "CONTRASENIA";
 	public static final String USUARIO_PERFIL = "PERFIL";
 	
 	//ISCONTRATO
-	public static final String objetoContrato="IsContrato";
+	public static final String objetoContrato="subSistemaBBDD.objetoBaseDatos.IsContrato";
 	public static final String TABLA_CONTRATO = "iscontrato";
 	public static final String ID_ISCONTRATO_ISUSUARIO_DNI = "ISUSUARIO_DNI";
 	public static final String CONTRATO_ISNOMINA_IDISNOMINA = "ISNOMINA_IDISNOMINA";
 	public static final String CONTRATO_TIPO = "TIPO";
 	
 	//ISPROFESOR
-	public static final String objetoProfesor="IsProfesor";
+	public static final String objetoProfesor="subSistemaBBDD.objetoBaseDatos.IsProfesor";
 	public static final String TABLA_PROFESOR = "isprofesor";
 	public static final String ID_ISPROFESOR_ISUSUARIO_DNI = "ISUSUARIO_DNI";
 	public static final String PROFESOR_ISAREA_IDISAREA = "ISAREA_IDISAREA";
@@ -81,7 +81,7 @@ public class Constantes {
 	public static final String PROFESOR_EMAIL = "EMAIL";
 	
 	//ISALUMNO
-	public static final String objetoAlumno="IsAlumno";
+	public static final String objetoAlumno="subSistemaBBDD.objetoBaseDatos.IsAlumno";
 	public static final String TABLA_ALUMNO = "isalumno";
 	public static final String ID_ISALUMNO_ISUSUARIO_DNI = "ISUSUARIO_DNI";
 	public static final String ALUMNO_NOMBRE = "NOMBRE";
@@ -94,7 +94,7 @@ public class Constantes {
 	public static final String ALUMNO_SEXO = "SEXO";
 	
 	//ISCURSO
-	public static final String objetoCurso="IsCurso";
+	public static final String objetoCurso="subSistemaBBDD.objetoBaseDatos.IsCurso";
 	public static final String TABLA_CURSO = "iscurso";
 	public static final String ID_ISCURSO_IDISCURSO = "IDISCURSO";
 	public static final String CURSO_ISPROFESOR_ISUSUARIO_DNI = "ISPROFESOR_ISUSUARIO_DNI";
@@ -107,13 +107,13 @@ public class Constantes {
 	public static final String CURSO_PRECIO = "PRECIO";
 		
 	//ISAVISOS_HAS_ISUSUARIO
-	public static final String objetoAvisoUsuario="IsAvisos_Has_IsUsuario";
+	public static final String objetoAvisoUsuario="subSistemaBBDD.objetoBaseDatos.IsAvisos_Has_IsUsuario";
 	public static final String TABLA_ISAVISOS_HAS_ISUSUARIO = "isavisos_has_isusuario";
 	public static final String ID_ISAVISOS_HAS_ISUSUARIO = "ISAVISOS_IDISAVISOS";
 	public static final String ID_ISAVISOS_HAS_ISUSUARIO_ISUSUARIO_DNI = "ISUSUARIO_DNI";	
 	
 	//ISCURSO_HAS_ISALUMNO
-	public static final String objetoCursoAlumno="IsCurso_Has_IsAlumno";
+	public static final String objetoCursoAlumno="subSistemaBBDD.objetoBaseDatos.IsCurso_Has_IsAlumno";
 	public static final String TABLA_ISCURSO_HAS_ISALUMNO = "iscurso_has_isalumno";
 	public static final String ID_HAS_ISCURSO_IDISCURSO = "ISCURSO_IDISCURSO";
 	public static final String ID_HAS_ISALUMNO_ISUSUARIO_DNI = "ISALUMNO_ISUSUARIO_DNI";
@@ -121,7 +121,7 @@ public class Constantes {
 	public static final String ISCURSO_HAS_ISALUMNO_NOTA_FINAL = "NOTA_FINAL";
 	
 	//ISHORARIO_HAS_ISAULA
-	public static final String objetoHorarioAula="IsHorario_Has-IsAula";
+	public static final String objetoHorarioAula="subSistemaBBDD.objetoBaseDatos.IsHorario_Has_IsAula";
 	public static final String TABLA_ISHORARIO_HAS_ISAULA = "ishorario_has_isaula";
 	public static final String ID_HAS_ISHORARIO_IDISHORARIO = "ISHORARIO_IDISHORARIO";
 	public static final String ID_HAS_ISAULA_IDISAULA = "ISAULA_IDISAULA";
