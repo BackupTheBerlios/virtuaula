16e2dcb0a5e0383eed9d61c879869037cb001e5e
albertois <albertois>
2006-05-27 23:24:24 +0000

    usando el creadorcontrolador

--- virtuaula/src/subSistemaControlador/gestorControlador/gestorControladorProfesor/GestorControladorOpAvisProf.java
+++ virtuaula/src/subSistemaControlador/gestorControlador/gestorControladorProfesor/GestorControladorOpAvisProf.java
@@ -4,6 +4,7 @@ package subSistemaControlador.gestorControlador.gestorControladorProfesor;
 import javax.servlet.http.HttpSession;
 
 import subSistemaControlador.controlador.Controlador;
+import subSistemaControlador.controlador.CreadorControlador;
 import subSistemaControlador.controlador.ControladorProfesor.controlEnviarAviso.EnviarAvisoProf;
 import subSistemaControlador.controlador.ControladorProfesor.controlEnviarAviso.IniEnviarAvisoProf;
 import subSistemaControlador.controlador.ControladorUsuario.ConsulEliAvis.BorraAviso;
@@ -23,12 +24,13 @@ public GestorControladorOpAvisProf(){
 		
 	}
 	public Controlador dameControlador(String perfilUsuario, String operacion, HttpSession sesion){
-		GestorControlador.tablaOperacionControlador.put("OP_AVIS_PROF",new ControladorOp_Avis());
-		GestorControlador.tablaOperacionControlador.put("MOSTRAR_AVISOS_PROF",new ControladorPasaPag());
-		GestorControlador.tablaOperacionControlador.put("BORRA_AVISO",new BorraAviso());
-		GestorControlador.tablaOperacionControlador.put("OP_AVIS_PROF_OPCIONES",new ControladorPasaPag());
-		GestorControlador.tablaOperacionControlador.put("INI_AVI_PROF",new IniEnviarAvisoProf());
-		GestorControlador.tablaOperacionControlador.put("ENV_AVI_PROF",new EnviarAvisoProf());
+		CreadorControlador c = new CreadorControlador();
+		GestorControlador.tablaOperacionControlador.put("OP_AVIS_PROF",c.crear(CreadorControlador.ControladorOp_Avis));
+		GestorControlador.tablaOperacionControlador.put("MOSTRAR_AVISOS_PROF",c.crear(CreadorControlador.ControladorPasaPag));
+		GestorControlador.tablaOperacionControlador.put("BORRA_AVISO",c.crear(CreadorControlador.BorraAviso));
+		GestorControlador.tablaOperacionControlador.put("OP_AVIS_PROF_OPCIONES",c.crear(CreadorControlador.ControladorPasaPag));
+		GestorControlador.tablaOperacionControlador.put("INI_AVI_PROF",c.crear(CreadorControlador.IniEnviarAvisoProf));
+		GestorControlador.tablaOperacionControlador.put("ENV_AVI_PROF",c.crear(CreadorControlador.EnviarAvisoProf));
 		Controlador controladorResultado=((Controlador)GestorControlador.tablaOperacionControlador.get(operacion));
 		
 		return controladorResultado;
