8686ad4002f59b3fffdb2a3618a5925f2dc10d36
albertois <albertois>
2006-05-27 09:52:20 +0000

    controladores para que un profesor mande un aviso

---
+++ virtuaula/src/subSistemaControlador/controlador/ControladorProfesor/controlEditarFicha/EnviarAvisoProf.java
@@ -0,0 +1,80 @@
+package subSistemaControlador.controlador.ControladorProfesor.controlEditarFicha;
+
+import gestores.GestorAvisos;
+
+import java.util.HashMap;
+
+import beans.CreadorBean;
+import beans.ObjetoBean;
+import beans.listaObjetoBeans.ListaObjetoBean;
+
+import subSistemaBBDD.utils.Constantes;
+import subSistemaControlador.controlador.Controlador;
+
+public class EnviarAvisoProf extends Controlador{
+
+	public void procesarEvento() {
+		
+		ObjetoBean aviso= (ObjetoBean)this.getSesion().getAttribute("beanAviso");
+		HashMap listaGrupos=(HashMap)this.getSesion().getAttribute("listagrupos");
+		//Esto no esta definido todavia
+		String op = (String)this.getSesion().getAttribute("clave");
+		Integer pos = (Integer)this.getSesion().getAttribute("posCombo");
+		int posi = pos.intValue();
+		ListaObjetoBean destino = (ListaObjetoBean)listaGrupos.get(op);
+		GestorAvisos GA= new GestorAvisos();
+		CreadorBean creador = new CreadorBean();
+		if (posi!=-1)
+		{
+			if (op.equals("secretaria"))
+			{//el aviso es para un grupo de secretarios(usuarios)
+				GA.avisoAGrupo(destino,aviso);
+			}
+			else if (op.equals("profesores"))
+			{//el aviso es para un grupo de profesores			
+				
+				int tama=destino.tamanio();
+				ListaObjetoBean dest = new ListaObjetoBean();
+				
+				for (int i=0;i<tama;i++)
+				{//me creo una lista de usuarios a partir de la lista de profesores
+					ObjetoBean usu=creador.crear(creador.Usuario);
+					usu.cambiaValor(Constantes.ID_ISUSUARIO_DNI ,destino.dameObjeto(i).dameValor(Constantes.ID_ISPROFESOR_ISUSUARIO_DNI));
+					dest.insertar(i,usu);
+					
+				}
+				GA.avisoAGrupo(dest,aviso);
+			}
+			else
+			{//el aviso es para un grupo de alumnos
+				int tamanio=destino.tamanio();
+				ListaObjetoBean desti = new ListaObjetoBean();
+				
+				for (int i=0;i<tamanio;i++)
+				{//me creo una lista de usuarios a partir de la lista de profesores
+					ObjetoBean usu=creador.crear(creador.Usuario);
+					usu.cambiaValor(Constantes.ID_ISUSUARIO_DNI ,destino.dameObjeto(i).dameValor(Constantes.ID_ISALUMNO_ISUSUARIO_DNI));
+					desti.insertar(i,usu);
+					
+				}
+				GA.avisoAGrupo(desti,aviso);
+			}
+		}
+		else //la pos es -1, es decir no hay ninguno elegido
+		{
+			ObjetoBean error = creador.crear(creador.Error);
+			error.cambiaValor(Constantes.CAUSA,"Debe elegir algun destinatario");
+			ListaObjetoBean listaerr= new ListaObjetoBean();
+			listaerr.insertar(0,error);
+			this.getSesion().setAttribute("error",listaerr);
+			this.setResuladooperacion("ERROR");
+			
+		}
+	}
+
+	public Controlador clonar() {
+		
+		return new EnviarAvisoProf();
+	}
+
+}
