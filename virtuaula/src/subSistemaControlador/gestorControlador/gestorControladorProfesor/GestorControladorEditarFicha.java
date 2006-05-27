ba820a0769e461bd48e8cb2ac7dc461d5ac35132
albertois <albertois>
2006-05-27 23:26:32 +0000

    usando el creador Controlador

--- virtuaula/src/subSistemaControlador/gestorControlador/gestorControladorProfesor/GestorControladorEditarFicha.java
+++ virtuaula/src/subSistemaControlador/gestorControlador/gestorControladorProfesor/GestorControladorEditarFicha.java
@@ -5,6 +5,7 @@ package subSistemaControlador.gestorControlador.gestorControladorProfesor;
 import javax.servlet.http.HttpSession;
 
 import subSistemaControlador.controlador.Controlador;
+import subSistemaControlador.controlador.CreadorControlador;
 import subSistemaControlador.controlador.ControladorProfesor.controlEditarFicha.ActualizarFichaAlumno;
 import subSistemaControlador.controlador.ControladorProfesor.controlEditarFicha.ControladorMostrarCursoProf;
 import subSistemaControlador.controlador.ControladorProfesor.controlEditarFicha.FichaAlumnoProf;
@@ -28,12 +29,12 @@ public class GestorControladorEditarFicha extends GestorControladorProfesor{
 	}
 	
 	public Controlador dameControlador(String perfilUsuario, String operacion, HttpSession sesion){
-
-		GestorControlador.tablaOperacionControlador.put("OP_MOSTRAR_CURSO_PROF",new ControladorMostrarCursoProf());
-		GestorControlador.tablaOperacionControlador.put("ListarAlumnosCurso",new ListarAlumnosCurso());
-		GestorControlador.tablaOperacionControlador.put("Ficha_Alumno_Prof",new FichaAlumnoProf());
-		GestorControlador.tablaOperacionControlador.put("ActualizarFichaAlumno",new ActualizarFichaAlumno());
-		GestorControlador.tablaOperacionControlador.put("PUB_NOTAS",new PublicarNotas());
+		CreadorControlador c = new CreadorControlador();
+		GestorControlador.tablaOperacionControlador.put("OP_MOSTRAR_CURSO_PROF",c.crear(CreadorControlador.ControladorMostrarCursoProf));
+		GestorControlador.tablaOperacionControlador.put("ListarAlumnosCurso",c.crear(CreadorControlador.ListarAlumnosCurso));
+		GestorControlador.tablaOperacionControlador.put("Ficha_Alumno_Prof",c.crear(CreadorControlador.FichaAlumnoProf));
+		GestorControlador.tablaOperacionControlador.put("ActualizarFichaAlumno",c.crear(CreadorControlador.ActualizarFichaAlumno));
+		GestorControlador.tablaOperacionControlador.put("PUB_NOTAS",c.crear(CreadorControlador.PublicarNotas));
 		Controlador controladorResultado=((Controlador)GestorControlador.tablaOperacionControlador.get(operacion));
 		
 		return controladorResultado;
