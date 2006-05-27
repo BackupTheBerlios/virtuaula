1d2ff86aeed117c9737a55da35b0b12356dc14d0
albertois <albertois>
2006-05-27 10:49:42 +0000

    Controladores para que el contable envie avisos

---
+++ virtuaula/src/subSistemaControlador/controlador/Contable/EnviarAvisoContable.java
@@ -0,0 +1,62 @@
+package subSistemaControlador.controlador.Contable;
+import gestores.GestorAvisos;
+
+import java.util.HashMap;
+
+import beans.CreadorBean;
+import beans.ObjetoBean;
+import beans.listaObjetoBeans.ListaObjetoBean;
+import subSistemaBBDD.utils.Constantes;
+import subSistemaControlador.controlador.Controlador;
+public class EnviarAvisoContable extends Controlador{
+
+	public void procesarEvento() {
+		
+		ObjetoBean aviso= (ObjetoBean)this.getSesion().getAttribute("beanAviso");
+		HashMap listaGrupos=(HashMap)this.getSesion().getAttribute("listagrupos");
+		//Esto no esta definido todavia
+		String op = (String)this.getSesion().getAttribute("clave");
+		Integer pos = (Integer)this.getSesion().getAttribute("posCombo");
+		int posi = pos.intValue();
+		ListaObjetoBean destino = (ListaObjetoBean)listaGrupos.get(op);
+		GestorAvisos GA= new GestorAvisos();
+		CreadorBean creador = new CreadorBean();
+		ListaObjetoBean errores= new ListaObjetoBean();
+		if (posi!=-1)
+		{
+			if (op.equals("secretaria"))
+			{//el aviso es para un grupo de secretarios(usuarios)
+				 errores=GA.avisoAGrupo(destino,aviso);
+			}
+						
+			if (!errores.esVacio())
+			{//tenemos errores al enviar el aviso
+				this.setResuladooperacion("ERROR");
+				this.getSesion().setAttribute("error",errores);
+			}
+			else
+			{//todo ha funcionado bien
+				this.setResuladooperacion("OK");
+				this.getSesion().removeAttribute("error");
+				
+			}
+		}
+		else //la pos es -1, es decir no hay ninguno elegido
+		{
+			ObjetoBean error = creador.crear(creador.Error);
+			error.cambiaValor(Constantes.CAUSA,"Debe elegir algun destinatario");
+			ListaObjetoBean listaerr= new ListaObjetoBean();
+			listaerr.insertar(0,error);
+			this.getSesion().setAttribute("error",listaerr);
+			this.setResuladooperacion("ERROR");
+			
+		}
+		
+	}
+
+	public Controlador clonar() {
+		
+		return new EnviarAvisoContable();
+	}
+
+}
---
+++ virtuaula/src/subSistemaControlador/controlador/Contable/IniEnviarAvisoContable.java
@@ -0,0 +1,53 @@
+package subSistemaControlador.controlador.Contable;
+import gestores.GestorUsuarios;
+import gestores.Profesorado;
+
+import java.util.HashMap;
+
+import beans.CreadorBean;
+import beans.ObjetoBean;
+import beans.listaObjetoBeans.ListaObjetoBean;
+import subSistemaBBDD.utils.Constantes;
+import subSistemaControlador.controlador.Controlador;
+public class IniEnviarAvisoContable extends Controlador{
+
+	public void procesarEvento() {
+		HashMap mapGrupos=new HashMap();
+		ObjetoBean usuario=(ObjetoBean)this.getSesion().getAttribute("beanUsuario");
+		CreadorBean creador = new CreadorBean();
+		ObjetoBean profesor = creador.crear(creador.Profesor);
+		profesor.cambiaValor(Constantes.ID_ISPROFESOR_ISUSUARIO_DNI,usuario.dameValor(Constantes.ID_ISUSUARIO_DNI));
+		GestorUsuarios GU = new GestorUsuarios();
+			
+		ListaObjetoBean listaSec =GU.dameUsuarios("secreataria");
+		
+		
+		if (!listaSec.esVacio())
+		{
+			mapGrupos.put("secretaria",listaSec);
+		}
+		
+		if ((!listaSec.esVacio()))
+		{	
+			this.setResuladooperacion("OK");
+			this.getSesion().setAttribute("listagrupos",mapGrupos);
+			this.getSesion().removeAttribute("error");
+		}
+		else 
+		{
+			ObjetoBean error = creador.crear(creador.Error);
+			error.cambiaValor(Constantes.CAUSA,"No hay usuarios a los que pueda mandar avisos");
+			ListaObjetoBean listaerr= new ListaObjetoBean();
+			listaerr.insertar(0,error);
+			this.getSesion().setAttribute("error",listaerr);
+			this.setResuladooperacion("ERROR");
+		}	
+		
+	}
+
+	public Controlador clonar() {
+		
+		return new IniEnviarAvisoContable();
+	}
+
+}
