68defeeb93bf2cd23c8d18a32433f2d217c250a0
albertois <albertois>
2006-05-27 23:29:05 +0000

    usando el creadorControlador

--- virtuaula/src/subSistemaControlador/gestorControlador/gestorControladorContable/GestorControladorOpAvisCont.java
+++ virtuaula/src/subSistemaControlador/gestorControlador/gestorControladorContable/GestorControladorOpAvisCont.java
@@ -8,6 +8,7 @@ import subSistemaControlador.controlador.ControladorUsuario.ConsulEliAvis.BorraA
 import subSistemaControlador.controlador.ControladorUsuario.ConsulEliAvis.ControladorOp_Avis;
 import subSistemaControlador.controlador.controlPasaPag.ControladorPasaPag;
 import subSistemaControlador.gestorControlador.GestorControlador;
+import subSistemaControlador.controlador.CreadorControlador;
 
 
 public class GestorControladorOpAvisCont extends GestorControladorContable{
@@ -17,10 +18,11 @@ public GestorControladorOpAvisCont(){
 		
 	}
 	public Controlador dameControlador(String perfilUsuario, String operacion, HttpSession sesion){
-		GestorControlador.tablaOperacionControlador.put("OP_AVIS_CONT",new ControladorOp_Avis());
-		GestorControlador.tablaOperacionControlador.put("MOSTRAR_AVISOS_CONT",new ControladorPasaPag());
-		GestorControlador.tablaOperacionControlador.put("BORRA_AVISO_CONT",new BorraAviso());
-		GestorControlador.tablaOperacionControlador.put("VOLVER_ANTERIOR_C",new ControladorPasaPag());
+		CreadorControlador c= new CreadorControlador();
+		GestorControlador.tablaOperacionControlador.put("OP_AVIS_CONT",c.crear(CreadorControlador.ControladorOp_Avis));
+		GestorControlador.tablaOperacionControlador.put("MOSTRAR_AVISOS_CONT",c.crear(CreadorControlador.ControladorPasaPag));
+		GestorControlador.tablaOperacionControlador.put("BORRA_AVISO_CONT",c.crear(CreadorControlador.BorraAviso));
+		GestorControlador.tablaOperacionControlador.put("VOLVER_ANTERIOR_C",c.crear(CreadorControlador.ControladorPasaPag));
 		Controlador controladorResultado=((Controlador)GestorControlador.tablaOperacionControlador.get(operacion));
 		
 		
