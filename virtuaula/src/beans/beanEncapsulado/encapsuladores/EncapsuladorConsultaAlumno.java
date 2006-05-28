b8e8dccf2be16082d5eac6aab643b3e0382703ec
jmusulin <jmusulin>
2006-05-28 14:02:12 +0000

    Esta es la version que os teneis que bajar.

--- virtuaula/src/beans/beanEncapsulado/encapsuladores/EncapsuladorConsultaAlumno.java
+++ virtuaula/src/beans/beanEncapsulado/encapsuladores/EncapsuladorConsultaAlumno.java
@@ -1,7 +1,5 @@
 package beans.beanEncapsulado.encapsuladores;
 import javax.servlet.http.HttpServletRequest;
-
-
 import beans.beanEncapsulado.EncapsuladorCompuesto;
 import beans.beanEncapsulado.encapsuladores.EncapsuladorClaveBean;
 import beans.beanEncapsulado.encapsuladores.EncapsuladorPosicionBean;
