816a21d985dbc17e10f5efd020469b71df9c4e4a
jmusulin <jmusulin>
2006-05-28 19:41:38 +0000

    Gestor para editar un alumno

---
+++ virtuaula/src/subSistemaControlador/gestorControlador/gestorControladorSecretaria/GestorControladorEditarAlumno.java
@@ -0,0 +1,23 @@
+package subSistemaControlador.gestorControlador.gestorControladorSecretaria;
+
+import javax.servlet.http.HttpSession;
+
+import subSistemaControlador.controlador.Controlador;
+import subSistemaControlador.controlador.CreadorControlador;
+import subSistemaControlador.gestorControlador.GestorControlador;
+
+public class GestorControladorEditarAlumno extends GestorControladorSecretaria {
+	
+	public Controlador dameControlador(String perfilUsuario, String operacion, HttpSession sesion){
+		CreadorControlador creadorcont=new CreadorControlador();
+		GestorControlador.tablaOperacionControlador.put("EDITAR_ALUM_INI",creadorcont.crear(CreadorControlador.ControladorPasaPag));
+		GestorControlador.tablaOperacionControlador.put("BUSCAR_ALUM_EDITAR",creadorcont.crear(CreadorControlador.ControladorBuscarAlumEditar));
+		GestorControlador.tablaOperacionControlador.put("SELEC_EDITAR_ALUMNO",creadorcont.crear(CreadorControlador.ControladorSelecEditarAlum));
+		GestorControlador.tablaOperacionControlador.put("CONF_EDITAR_ALUMNO",creadorcont.crear(CreadorControlador.ControladorConfEditarAlum));
+		GestorControlador.tablaOperacionControlador.put("FIN_EDITAR_ALUMNO",creadorcont.crear(CreadorControlador.ControladorFinEditarAlum));
+		Controlador controladorResultado=((Controlador)GestorControlador.tablaOperacionControlador.get(operacion));
+		
+		return controladorResultado;
+	}
+
+}
