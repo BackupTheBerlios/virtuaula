761f3d11ed382986a3fa33140c694b9a46f48caa
albertois <albertois>
2006-05-29 00:12:34 +0000

    pone aviso a "N" en el campo activo

--- virtuaula/src/subSistemaControlador/gestorControlador/gestorControladorContable/GestorControladorOpAvisCont.java
+++ virtuaula/src/subSistemaControlador/gestorControlador/gestorControladorContable/GestorControladorOpAvisCont.java
@@ -20,7 +20,7 @@ public GestorControladorOpAvisCont(){
 	public Controlador dameControlador(String perfilUsuario, String operacion, HttpSession sesion){
 		CreadorControlador c= new CreadorControlador();
 		GestorControlador.tablaOperacionControlador.put("OP_AVIS_CONT",c.crear(CreadorControlador.ControladorOp_Avis));
-		GestorControlador.tablaOperacionControlador.put("MOSTRAR_AVISOS_CONT",c.crear(CreadorControlador.ControladorPasaPag));
+		GestorControlador.tablaOperacionControlador.put("MOSTRAR_AVISOS_CONT",c.crear(CreadorControlador.ControladorLeerAviso));
 		GestorControlador.tablaOperacionControlador.put("BORRA_AVISO_CONT",c.crear(CreadorControlador.BorraAviso));
 		GestorControlador.tablaOperacionControlador.put("VOLVER_ANTERIOR_C",c.crear(CreadorControlador.ControladorPasaPag));	
 		GestorControlador.tablaOperacionControlador.put("INI_AVI_CONT",c.crear(CreadorControlador.IniEnviarAvisoContable));
