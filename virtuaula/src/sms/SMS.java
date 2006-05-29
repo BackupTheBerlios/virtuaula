b7382eab2ef2d8af5879d3445ce12c773eb30453
jmusulin <jmusulin>
2006-05-29 13:53:54 +0000

    Añadida clase sms para enviar un sms al alumno cuando se matricula, cambiado controlador matricula para que envie un sms al alumno al matricularle

---
+++ virtuaula/src/sms/SMS.java
@@ -0,0 +1,111 @@
+package sms;
+
+import java.io.*;
+import java.net.*;
+
+public class SMS {
+
+ private String socket_envia(String host, int puerto, String output) {
+
+        Socket smtpSocket = null;  
+        DataOutputStream os = null;
+        BufferedReader is= null;
+        String input = new String();
+
+        try {
+            smtpSocket = new Socket(host, puerto);
+            os = new DataOutputStream(smtpSocket.getOutputStream());
+            is = new BufferedReader(new InputStreamReader(smtpSocket.getInputStream()));
+
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: hostname");
+        } catch (IOException e) {
+            return(null);
+        }
+
+        if (smtpSocket != null && os != null && is != null) {
+            try {
+
+
+                os.writeBytes(output);
+
+                String responseLine;
+                while ((responseLine = is.readLine()) != null) {
+                     input=input.concat(responseLine+"\r\n");
+                }
+
+                os.close();
+                is.close();
+                smtpSocket.close();  
+            } catch (UnknownHostException e) {
+                 return(null);
+           } catch (IOException e) {
+                 return(null);
+           }
+        
+          return(input);
+        }
+    return(null);
+
+    }          
+
+public int envia(String numero, String texto) {
+
+  String peticion=new String();
+  String respuesta;
+  String user="";
+  String password="";
+
+  String post_data=new String("user="+user+"&pwd="+password+"&lang=es&fwd=home&p=");
+
+  peticion=peticion.concat("POST /www/checklogin HTTP/1.0\r\n");
+  peticion=peticion.concat("Host: secure2.peoplecall.com\r\n");
+  peticion=peticion.concat("Content-Length: "+post_data.length()+"\r\n");
+  peticion=peticion.concat("Content-type: application/x-www-form-urlencoded\r\n");
+  peticion=peticion.concat("\r\n"); 
+  peticion=peticion.concat(post_data);
+
+  //System.out.println(peticion);
+
+  respuesta=socket_envia("secure2.peoplecall.com",80,peticion);
+  
+  if (respuesta==null) return(-1);
+
+//  System.out.println(respuesta);
+
+   // Buscar la cookie
+  int begin=respuesta.indexOf("JSESSIONID=");
+  int end=respuesta.indexOf("; Path=/www");
+
+  String cookie_str=respuesta.substring(begin,end);
+  //System.out.println(cookie_str);
+
+  texto=texto.replaceAll(" ","+");
+  
+  post_data=new String("body="+texto+"&destination_number=34"+numero);
+
+  peticion=new String();
+  peticion=peticion.concat("POST /www/statusmsend HTTP/1.0\r\n");
+  peticion=peticion.concat("Host: secure2.peoplecall.com\r\n");
+  peticion=peticion.concat("Content-Length: "+post_data.length()+"\r\n");
+  peticion=peticion.concat("Content-type: application/x-www-form-urlencoded\r\n");
+  peticion=peticion.concat("Cookie: "+cookie_str+"\r\n");
+  peticion=peticion.concat("\r\n");
+  peticion=peticion.concat(post_data);
+
+  //System.out.println(peticion);
+  respuesta=socket_envia("secure2.peoplecall.com",80,peticion);
+
+  if (respuesta==null) return(-1);
+
+  //System.out.println(respuesta);
+  return(0);
+
+
+
+}
+
+
+
+
+}
--- virtuaula/src/subSistemaControlador/controlador/ControladorSecretaria/controlMatricular/ControladorMatricula.java
+++ virtuaula/src/subSistemaControlador/controlador/ControladorSecretaria/controlMatricular/ControladorMatricula.java
@@ -1,10 +1,13 @@
 package subSistemaControlador.controlador.ControladorSecretaria.controlMatricular;
 
+
 import gestores.GestorAlumnos;
+import sms.SMS;
 import beans.ObjetoBean;
 import beans.CreadorBean;
 import subSistemaControlador.controlador.Controlador;
 import beans.listaObjetoBeans.ListaObjetoBean;
+import subSistemaBBDD.utils.*;
 /**
  * 
  * @author Alberto Macho
@@ -23,6 +26,7 @@ public class ControladorMatricula extends ControladorMatricular{
 		
 		ListaObjetoBean listaerror=GA.marticularAlumno(alumno,curso,usuario);
 		//si los datos introducidos son correctos
+		
 		if (listaerror.esVacio())
 		{
 			
@@ -30,6 +34,12 @@ public class ControladorMatricula extends ControladorMatricular{
 			//metemos en sesion el usuario para que en la sig pagina muestre el usuario
 			//y la contraseña.
 			this.getSesion().setAttribute("usuario",usuario);
+			SMS sms= new SMS();
+			String mensaje=" Bienvenido a virtuAula "+ alumno.dameValor(Constantes.ALUMNO_NOMBRE) +" Tu login es:"+
+			usuario.dameValor(Constantes.ID_ISUSUARIO_DNI)+ " Tu contraseña es:" +usuario.dameValor(Constantes.USUARIO_CONTRASENIA);
+			String numero= alumno.dameValor(Constantes.ALUMNO_TELEFONO);
+			sms.envia(numero,mensaje);
+			
 			//borramos la listaeror porque ya lo hacemos correctamente.
 			this.getSesion().removeAttribute("error");
 		}
