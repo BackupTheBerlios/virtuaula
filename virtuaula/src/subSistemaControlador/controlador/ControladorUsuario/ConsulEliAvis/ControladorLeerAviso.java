6b2f8f51593e7f2553b0573ce1d5d2e74d7ff069
albertois <albertois>
2006-05-29 00:10:19 +0000

    controlador para que al leer el aviso ponga en el campo Activo "N"

---
+++ virtuaula/src/subSistemaControlador/controlador/ControladorUsuario/ConsulEliAvis/ControladorLeerAviso.java
@@ -0,0 +1,34 @@
+package subSistemaControlador.controlador.ControladorUsuario.ConsulEliAvis;
+
+import subSistemaBBDD.utils.Constantes;
+import subSistemaControlador.controlador.Controlador;
+import subSistemaControlador.controlador.ControladorUsuario.ControladorUsuario;
+import beans.listaObjetoBeans.*;
+import beans.ObjetoBean;
+import gestores.GestorAvisos;;
+
+public class ControladorLeerAviso extends ControladorUsuario{
+
+	public void procesarEvento() {
+		
+		ListaObjetoBean listaObjetoBean =((ListaObjetoBean)sesion.getAttribute("listaaviso"));
+        Integer posicionAviso = ((Integer)sesion.getAttribute("posAviso"));
+      	ObjetoBean aviso = null;
+      	
+        if (listaObjetoBean != null && !listaObjetoBean.esVacio() && posicionAviso != null){
+        	if (posicionAviso.intValue() <= listaObjetoBean.tamanio()) {
+        		aviso =  listaObjetoBean.dameObjeto(posicionAviso.intValue()); 
+        		aviso.cambiaValor(Constantes.AVISOS_ACTIVO,"N");
+        		GestorAvisos GA = new GestorAvisos();
+        		GA.editarAviso(aviso);
+        	}}
+        this.setResuladooperacion("SIG");
+		
+	}
+
+	public Controlador clonar() {
+		
+		return new ControladorLeerAviso();
+	}
+
+}
