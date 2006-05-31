39f4bafbb3cc1e1f12b7f673a44fcbf74ccb0032
albertois <albertois>
2006-05-31 18:45:37 +0000

    comentaios

--- virtuaula/src/subSistemaControlador/controlador/ControladorRecHumanos/ControladorRecHumanos.java
+++ virtuaula/src/subSistemaControlador/controlador/ControladorRecHumanos/ControladorRecHumanos.java
@@ -1,7 +1,12 @@
 package subSistemaControlador.controlador.ControladorRecHumanos;
 
 import subSistemaControlador.controlador.Controlador;
-
+/**
+ * 
+ * Controlador del que heredan todos los relacionados con las 
+ * operaciones del empleado de recursos humanos
+ *
+ */
 abstract public class ControladorRecHumanos  extends Controlador {
 
 
