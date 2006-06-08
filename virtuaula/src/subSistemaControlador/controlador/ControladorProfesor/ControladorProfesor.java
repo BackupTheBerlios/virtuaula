eab8b0b14b56301388d6d3f02691ec8c7bf08481
albertois <albertois>
2006-06-08 08:59:53 +0000

    commit

--- virtuaula/src/subSistemaControlador/controlador/ControladorProfesor/ControladorProfesor.java
+++ virtuaula/src/subSistemaControlador/controlador/ControladorProfesor/ControladorProfesor.java
@@ -10,7 +10,10 @@ import subSistemaControlador.controlador.Controlador;
 abstract public class ControladorProfesor extends Controlador {
 
 
-
+	/**
+	* Este metodo nos modifica el resultadooperacion para indicar a la pagina que
+	* tiene que pasar. 
+	*/
 	abstract public void procesarEvento(); 
 
 	
